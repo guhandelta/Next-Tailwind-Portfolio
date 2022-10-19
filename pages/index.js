923bbb99680e1539c4b9012ab03c55daf949d646
Kala Foods <guhan.delta@gmail.com>
2022-10-19 07:46:21 -0400

    Completed a responsive Navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Navbar } from '../components'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>App Works!!......</h1>
+      <Navbar />
 
     </div>
   )
